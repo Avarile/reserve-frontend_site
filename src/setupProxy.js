12bc7362c2ca13f931e7b2fa164d7e49dda751c6
Avarile <avarile>
2023-10-11 10:15:19 +1100

    update: query for reservation

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // target: 'http://13.211.212.227:9007/',
-      target: 'http://localhost:9000/',
+      target: 'http://13.211.212.227:9007/',
+      // target: 'http://localhost:9000/',
       changeOrigin: true,
     })
   );
