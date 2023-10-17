d5e21a4badee0601bb80d117073cad7f9b296db6
Avarile <Avarile@gmail.com>
2023-10-18 10:03:37 +1100

    update: keys

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://13.211.212.227:9007/',
+      target: 'https://wildlifesearch.org.au/',
       // target: 'http://localhost:9000/',
       changeOrigin: true,
     })
